0adaad06d3776a37c9e1543c5ea11d661bb4d647
malone12 <malone12@illinois.edu>
2022-05-17 14:19:00 -0500

    Added LMoD Usage Log Parser

---
+++ lmod_usage_log_parse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tfile=$(mktemp /tmp/lmp.XXXXXXX)
+log_path=""
+
+## Last Minute
+## Get any log lines from past 60 seconds and chunk it to only what we need
+date_filter=$(date +"%b %d %H:%M:" -d -1minute)
+cat ${log_path} | grep "${date_filter}" | cut -d'=' -f 2,4,6 | sed -e 's/module=//g' -e 's/host=//g' > "${tfile}"
+
+## Parse lines
+while read -r line; do
+	IFS=" " read -r username mod_path load_time_raw <<< "${line}"
+	load_time=$(echo "${load_time_raw}" | sed 's/\.//g')
+	echo "lmod_usage_stats,username=${username},module=${mod_path} count=1 ${load_time}000"
+done < ${tfile}
+
+## Cleanup
+rm -rf ${tfile}
