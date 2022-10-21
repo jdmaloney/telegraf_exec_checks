b15eebefb61d6ea026839316fe1d77a62da85129
malone12 <malone12@illinois.edu>
2022-10-21 10:40:25 -0500

    Improved performance

--- headnode_user_resources.sh
+++ headnode_user_resources.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+tfile=$(mktemp /tmp/user_resource.XXXXXXXXX)
 users=($(ps axo user:20 | sort -u | grep -v USER))
 
 for u in ${users[@]}
 do
-	cpu_percent=$(ps axo user:20,pid,pcpu,pmem | egrep ^${u} | awk '{print $3}' | paste -s -d+ - | bc)
-	mem_percent=$(ps axo user:20,pid,pcpu,pmem | egrep ^${u} | awk '{print $4}' | paste -s -d+ - | bc)
+	ps axo user:20,pid,pcpu,pmem > "${tfile}"
+	cpu_percent=$(egrep ^${u} ${tfile} | awk '{print $3}' | paste -s -d+ - | bc)
+	mem_percent=$(egrep ^${u} ${tfile} | awk '{print $4}' | paste -s -d+ - | bc)
 	mem_kb=$(ps -U ${u} --no-headers -o rss | awk '{ sum+=$1} END {print int(sum)}')
-	num_processes=$(ps aux | awk -v user=${u} '$1 == user {print $0}' | wc -l)
+	num_processes=$(awk -v user=${u} '$1 == user {print $0}' ${tfile} | wc -l)
 	echo "login_node_user_resource_usage,user=${u} cpu_percent=${cpu_percent},mem_percent=${mem_percent},mem_kb=${mem_kb},num_processes=${num_processes}"
 done
+
+rm -rf ${tfile}
